87530d223b1bd8de0705afedd0cdb3d86756fb59
victorocna <victorocnarescu@gmail.com>
2025-05-08 19:22:55 +0300

    Update api url

--- src/fetch-ecb-data.js
+++ src/fetch-ecb-data.js
@@ -1,5 +1,5 @@
 const fetchEcbData = async (startDate, endDate) => {
-  const ecbApiUrl = `https://sdw-wsrest.ecb.europa.eu/service/data/EXR/D..EUR.SP00.A`;
+  const ecbApiUrl = `https://data-api.ecb.europa.eu/service/data/EXR/D..EUR.SP00.A`;
   const queryParams = new URLSearchParams({
     startPeriod: startDate,
     endPeriod: endDate,
